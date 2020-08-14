bdb8ebecd77b7797bae19c1f5e5f0826a48bd1c6
Heitor França <heitoroliveira8@hotmail.com>
2020-08-14 17:58:01 -0300

    Modulo 3 realizando Todo

--- Modulo 3/ToDos.js
+++ Modulo 3/ToDos.js
@@ -1,1 +1,20 @@
-alert("Funcionou");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var ToDos = [
+   "Fazer café",
+   "Estudar JavaScript",
+   "Acessar Comunidade RocketSeat"
+];
+
+function renderToDos(){
+	for(ToDo of ToDos){
+		var ToDoElement = document.createElement("li");
+		var ToDoText = document.createTextNode(ToDo);
+
+		ToDoElement.appendChild(ToDoText);
+		listElement.appendChild(ToDoElement);
+			}
+}
+renderToDos();
