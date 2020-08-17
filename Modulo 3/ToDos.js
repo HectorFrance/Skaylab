7b137d2074be2aeac9a407fc20f19f294017227b
Heitor França <heitoroliveira8@hotmail.com>
2020-08-17 20:49:29 -0300

    Modulo 3 completo

--- Modulo 3/ToDos.js
+++ Modulo 3/ToDos.js
@@ -2,19 +2,52 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var ToDos = [
-   "Fazer café",
-   "Estudar JavaScript",
-   "Acessar Comunidade RocketSeat"
-];
+var ToDos = JSON.parse(localStorage.getItem("list_ToDos")) || [];
 
 function renderToDos(){
+	listElement.innerHTML =  "";
+
 	for(ToDo of ToDos){
 		var ToDoElement = document.createElement("li");
 		var ToDoText = document.createTextNode(ToDo);
 
+		var linkElement = document.createElement("a");
+		linkElement.setAttribute("href","#");
+
+		var pos = ToDos.lastIndexOf(ToDo);
+		linkElement.setAttribute("onclick", "deleteToDo("+pos+")");
+
+		var linkText = document.createTextNode("Excuir");
+
+		linkElement.appendChild(linkText);
+
 		ToDoElement.appendChild(ToDoText);
+		ToDoElement.appendChild(linkElement);
+
 		listElement.appendChild(ToDoElement);
 			}
 }
+
 renderToDos();
+
+function addToDo(){
+	var ToDoText = inputElement.value;
+
+	ToDos.push(ToDoText);
+	inputElement.value = "";
+	renderToDos();
+	saveToStorage();
+}
+
+buttonElement.onclick = addToDo;
+
+function deleteToDo(pos){
+	ToDos.splice(pos , 1);
+
+	renderToDos();
+	saveToStorage();
+}
+
+function saveToStorage(){
+	localStorage.setItem("list_ToDos", JSON.stringify(ToDos));
+}
