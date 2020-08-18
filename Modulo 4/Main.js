6cde22c9eb5ed8344d78a521acf1b1a2bcf72526
Heitor França <heitoroliveira8@hotmail.com>
2020-08-18 13:40:44 -0300

    Promise e Requisições AJAX

---
+++ Modulo 4/Main.js
@@ -0,0 +1,39 @@
+/*var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.github.com/users/hectorfrance");
+xhr.send(null);
+
+xhr.onreadystatechange = function(){
+	if (xhr.readyState === 4) {
+		console.log(JSON.parse(xhr.responseText));
+	}
+}
+*/
+var minhaPromise = function(){
+	return new Promise(function(resolve, reject){
+		xhr2 = new XMLHttpRequest();
+		xhr2.open("GET", "https://api.gith51ub.com/users/hectorfrance");
+		xhr2.send(null);
+
+		xhr2.onreadystatechange = function(){
+			if (xhr2.readyState === 4) {
+				if (xhr2.status ===200) {
+					resolve(JSON.parse(xhr2.responseText));
+				}else{
+					reject("Erro na requisição")
+				}
+			}
+		}
+	});
+}
+
+//var resultado = minhaPromise();
+//console.log(resultado);
+
+minhaPromise()
+.then(function(response){
+	console.log(response);
+})
+.catch(function(error){
+	console.warn(error);
+})
